cc4168ac211f8131e27674747345690b3c3d53cb
zhangb <zhangbo9269@gmail.com>
2015-11-18 09:47:16 +0800

    modify
    Signed-off-by: zhangb <zhangbo9269@gmail.com>

--- driver.c
+++ driver.c
@@ -7,39 +7,39 @@
 
 #include "driver.h"
 
-/**/
+/*参数*/
 static char *whom ="zhangb";
 static int howmany =1;
 module_param(whom,charp,S_IRUGO);
 module_param(howmany,int,S_IRUGO);
 
-/**/
+/*获取设备号，并注册设备*/
 static int getIndo(void)  
 {
-    int rt = -1;   
+    int ret = -1;   
     dev_t dev = -1 ;
  
     if(major)
     {
        dev = MKDEV(major , 0);  
-       rt = register_chrdev_region(dev,nr,"driver");  
+       ret = register_chrdev_region(dev,nr,"driver");  
     }  
     else
     {
-        rt = alloc_chrdev_region(&dev,0,nr,"driver"); 
+        ret = alloc_chrdev_region(&dev,0,nr,"driver"); 
         major = MAJOR(dev);  
     }
-    return rt ; 
+    return ret ; 
 } 
 
-/**/
+/*释放设备*/
 static int clearIndo(void)
 {
     unregister_chrdev_region(MKDEV(major , 0) ,nr);  
     return 0 ;
 }
 
-/**/
+/*初始化设备*/
 static int __init testinit(void)
 {
     printk(KERN_ALERT "module init\n"); 
@@ -47,7 +47,7 @@ static int __init testinit(void)
 
 }
 
-/**/
+/*清除设备*/
 static void __exit testexit(void)
 {
     printk(KERN_ALERT "module exit\n");
@@ -57,7 +57,7 @@ static void __exit testexit(void)
 module_init(testinit);  
 module_exit(testexit); 
 
-/**/
+/*说明*/
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("zhangb"); 
 MODULE_VERSION("v1.0");  
--- driver.h
+++ driver.h
@@ -1,3 +1,5 @@
 #include <linux/types.h> 
+//设备个数
 int nr = 4;
+//主设备号
 dev_t major = 0 ; 
